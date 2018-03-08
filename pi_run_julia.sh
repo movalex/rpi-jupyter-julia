ebba4f02741f59cb187cbe219264b3f8e9c17800
Alex Bogomolov <mail@abogomolov.com>
2018-03-08 10:18:47 +0300

    update sample launch script

--- pi_run_julia.sh
+++ pi_run_julia.sh
@@ -1,8 +1,4 @@
-IMAGE=${1:-'movalex/rpi-jupyter-julia'}
+IMAGE=${1:-'movalex/rpi-jupyter-julia:latest'}
 HOST_WORK_DIR=$HOME/docker/work
 
-docker run -d -p 8888:8888\
-    --name jp_pi \
-    -v /etc/localtime:/etc/localtime:ro \
-    -v $HOME/.jupyter/jupyter_notebook_config.py:/home/jovyan/.jupyter/jupyter_notebook_config.py \
-    -v $HOST_WORK_DIR:/home/jovyan/work "$IMAGE"
+docker run -d -p 8889:8888 --name jp -v /etc/localtime:/etc/localtime:ro -v $HOME/.jupyter/jupyter_notebook_config.py:/home/jovyan/.jupyter/jupyter_notebook_config.py -v $HOST_WORK_DIR:/home/jovyan/work "$IMAGE"
