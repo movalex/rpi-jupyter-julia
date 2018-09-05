f5a030585b2cb2ce277c5cea5bbcbd6d2b590e7a
Alex Bogomolov <mail@abogomolov.com>
2018-09-05 10:03:49 +0300

    change run helper script

--- pi_run_julia.sh
+++ pi_run_julia.sh
@@ -1,8 +1,4 @@
-IMAGE=${1:-'movalex/rpi-jupyter-julia:latest'}
-    HOST_WORK_DIR=$HOME/docker/work
-    docker run -d -p 8888:8888\
-    --name jp\
-    -v /etc/localtime:/etc/localtime:ro\
-    -v $HOST_WORK_DIR:/home/jovyan/work "$IMAGE"\
-    -v $HOME/.jupyter/jupyter_notebook_config.py:/home/jovyan/.jupyter/jupyter_notebook_config.py
-    
+IMAGE=${1:-'movalex/rpi-jupyter-julia:1.0.0'}
+HOST_WORK_DIR=$HOME/docker/work
+
+docker run -d --restart unless-stopped -p 8889:8888  --name jpj -v $HOME/.jupyter/jupyter_notebook_config.py:/home/jovyan/.jupyter/jupyter_notebook_config.py -v /etc/localtime:/etc/localtime:ro -v $HOST_WORK_DIR:/home/jovyan/work "$IMAGE"
